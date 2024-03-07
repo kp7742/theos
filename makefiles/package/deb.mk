3b565fbf1be6dde6224f255f797bb7fb24aad1e8
Kuldip Patel <patel.kuldip91@gmail.com>
2024-03-08 04:34:04 +0530

    Removed the comment

--- makefiles/package/deb.mk
+++ makefiles/package/deb.mk
@@ -27,7 +27,6 @@ THEOS_PACKAGE_ARCH := $(shell grep -i "^Architecture:" "$(_THEOS_DEB_PACKAGE_CON
 THEOS_PACKAGE_BASE_VERSION := $(shell grep -i "^Version:" "$(_THEOS_DEB_PACKAGE_CONTROL_PATH)" | cut -d' ' -f2-)
 
 $(THEOS_STAGING_DIR)/DEBIAN:
-	# Force permission mode to 755 for Termux
 	$(ECHO_NOTHING)mkdir -m 755 -p "$(THEOS_STAGING_DIR)/DEBIAN"$(ECHO_END)
 ifeq ($(_THEOS_HAS_STAGING_LAYOUT),1) # If we have a layout directory, copy layout/DEBIAN to the staging directory.
 	$(ECHO_NOTHING)[ -d "$(THEOS_LAYOUT_DIR)/DEBIAN" ] && rsync -a "$(THEOS_LAYOUT_DIR)/DEBIAN/" "$(THEOS_STAGING_DIR)/DEBIAN" $(_THEOS_RSYNC_EXCLUDE_COMMANDLINE) || true$(ECHO_END)
